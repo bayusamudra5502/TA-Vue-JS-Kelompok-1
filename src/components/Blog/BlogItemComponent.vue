5e1d5f9148a95f03007b83dcb744382e56b9ca0a
Farham Harvianto <lipi.farham@gmail.com>
2021-08-08 14:50:44 +0700

    Blog Farham

---
+++ src/components/Blog/BlogItemComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-card class="box-card" shadow="hover" style="margin-bottom: 10px">
+        <div>
+            <h3>{{ blog.title }}</h3>
+            <p>{{ blog.description.substring(0, 15) }}</p>
+            <img class="image" :src="blog.photo ? apiDomain + blog.photo : 'https://picsum.photos/200/300'" height="200px" style="float: right"/>
+        </div>
+    </el-card>
+</template>
+
+<script>
+//import { defineComponent } from '@vue/composition-api'
+
+export default {
+    data : () => ({
+         apiDomain : 'http://demo-api-vue.sanbercloud.com' ,
+    }),
+    props : ['blog']
+}
+</script>
