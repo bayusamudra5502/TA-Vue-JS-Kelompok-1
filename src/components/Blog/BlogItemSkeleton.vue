134934a3525cbae0d27e1b2cbb2288bca4f213a5
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-08-08 19:43:36 +0700

    Menambahkan Skeleton

---
+++ src/components/Blog/BlogItemSkeleton.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-skeleton :loading="loading" animated>
+    <template slot="template">
+      <el-card class="box-card" shadow="hover" style="margin-bottom: 10px">
+        <div class="content">
+          <el-skeleton-item
+            variant="h1"
+            style="width: 50%; margin-bottom: 20px"
+          />
+          <el-skeleton-item variant="text" />
+          <el-skeleton-item variant="text" />
+          <el-skeleton-item variant="text" />
+          <el-skeleton-item variant="text" />
+          <el-skeleton-item variant="text" />
+
+          <el-skeleton-item variant="text" />
+        </div>
+        <div class="image">
+          <el-skeleton-item variant="image" style="width: 100%; height: 100%" />
+        </div>
+      </el-card>
+    </template>
+    <template>
+      <slot />
+    </template>
+  </el-skeleton>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: Boolean,
+  },
+};
+</script>
